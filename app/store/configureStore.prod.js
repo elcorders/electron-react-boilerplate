e165aba7bf9853b82f6a853a5afbe179d4947234
Mark Tompkins <mets_dev@outlook.com>
2017-07-02 12:28:46 -0400

    Add configureStore.prod changes

--- app/store/configureStore.prod.js
+++ app/store/configureStore.prod.js
@@ -1,17 +1,30 @@
 // @flow
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import { createBrowserHistory } from 'history';
+import { createMemoryHistory } from 'history';
 import { routerMiddleware } from 'react-router-redux';
+import { forwardToMain, forwardToRenderer, triggerAlias, replayActionMain, replayActionRenderer } from 'electron-redux';
 import rootReducer from '../reducers';
 import type { counterStateType } from '../reducers/counter';
 
-const history = createBrowserHistory();
+// const history = createBrowserHistory();
+const history = createMemoryHistory();
 const router = routerMiddleware(history);
-const enhancer = applyMiddleware(thunk, router);
+// const enhancer = applyMiddleware(thunk, router);
+let enhancer;
 
-function configureStore(initialState?: counterStateType) {
-  return createStore(rootReducer, initialState, enhancer);
+// function configureStore(initialState?: counterStateType) {
+//   return createStore(rootReducer, initialState, enhancer);
+// }
+
+function configureStore(initialState, scope: string = 'main') {
+  scope === 'main' ? enhancer = applyMiddleware(triggerAlias, thunk, router, forwardToRenderer) : enhancer = applyMiddleware(forwardToMain, thunk, router);
+
+  const store = createStore(rootReducer, initialState, enhancer);
+
+  scope === 'main' ? replayActionMain(store) : replayActionRenderer(store);
+
+  return store;
 }
 
 export default { configureStore, history };
